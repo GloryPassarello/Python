c1948e609b274226bf3229fb8f9a6ac352ba71a3
GloryPassarello <gloria.passarello@virgilio.it>
2017-01-31 17:53:59 +0100

    parsing the net

--- 3of5/exercise/week4/reading_table.py
+++ 3of5/exercise/week4/reading_table.py
@@ -4,12 +4,21 @@ from BeautifulSoup import *
 url = raw_input('Enter - ')
 html = urllib.urlopen(url).read()
 
+print html
+
 soup = BeautifulSoup(html)
 # Retrieve all of the anchor tags
-tags = soup('a')
+tags = soup('span')
+count = 0
+summary = int()
 for tag in tags:
     # Look at the parts of a tag
-    print 'TAG:',tag
-    print 'URL:',tag.get('href', None)
-    print 'Contents:',tag.contents[0]
-    print 'Attrs:',tag.attrs
+    #print 'TAG:',tag
+    #print 'URL:',tag.get('href', None)
+    #print 'Contents:',tag.contents[0]
+    #print 'Attrs:',tag.attrs
+    num = int(tag.contents[0])
+    count = count + 1 
+    summary = summary + num
+print 'Count: ', count
+print 'Sum: ', summary
---
+++ 3of5/exercise/week4/reading_table2.py
@@ -0,0 +1,25 @@
+import urllib
+from BeautifulSoup import *
+
+url = raw_input('Enter url - ')
+position = raw_input('Enter position - ')
+process = raw_input('Enter the number of times you want do repeat the process - ')
+
+num = int(position)
+process = int(process)
+#print type(position)
+
+
+print 'Retreiving: -', url 
+times = 0
+while times <= process:
+	html = urllib.urlopen(url).read()
+	soup = BeautifulSoup(html)
+	tags = soup('a')
+	times = times + 1
+	count= 0
+	for tag in tags:
+	    ref_url = tag.get('href', None)
+	    count = count + 1
+	    if count is num:
+		   	print  'Retreiving: -', ref_url
